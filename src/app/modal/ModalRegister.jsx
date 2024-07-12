b5e93c11a1c6ed9c5643e4a2763ef6047a1e8188
Jair Cornejo <jaircornejo61@gmail.com>
2024-07-12 17:34:12 -0300

    cree el modalRegistrarse

--- src/app/modal/ModalRegister.jsx
+++ src/app/modal/ModalRegister.jsx
@@ -1,5 +1,5 @@
-const ModalLogin = ({openModal,setOpenModal}) => {
-    
+const ModalRegister = ({openModal,setOpenModal}) => {
+
     let verModal;
     if(openModal==false){
         verModal="opacity-0 pointer-events-none";
@@ -14,13 +14,19 @@ const ModalLogin = ({openModal,setOpenModal}) => {
             <div className={`absolute inset-0 flex items-center justify-center z-10 bg-[#242d34] bg-opacity-30 transition-all duration-500 ${verModal}`}>
                 <div className="flex flex-col pb-16 pt-9 px-24 rounded-xl">
                     <i class="bi bi-x text-4xl text-end mb-4 cursor-pointer" onClick={()=>setOpenModal(false)}></i>
-                    <h1 className="text-center font-semibold text-4xl pb-8">Inicia sesión en X</h1>
+                    <h1 className="text-center font-semibold text-4xl pb-8">Registrarse en X</h1>
                     <form action="" className="flex flex-col">
-                        <label className="text-lg pb-4">Correo electronico</label>
+                        <label className="text-lg pb-4">Nombre</label>
+                        <input type="text" className={classInputs}/>
+                        <label className="text-lg pb-4 mt-4">Nombre de Usuario</label>
+                        <input type="text" className={classInputs}/>
+                        <label className="text-lg pb-4 mt-4">Correo electronico</label>
                         <input type="email" className={classInputs}/>
                         <label className="text-lg mt-4 pb-4">Contraseña</label>
                         <input type="password" className={classInputs}/>
-                        <input type="submit" value="Ingresar" className="text-black bg-white mt-6 py-1 rounded-xl text-lg from-transparent transition-all duration-300 cursor-pointer hover:bg-[#cbcbcb]"/>
+                        <label className="text-lg mt-4 pb-4">Repetir Contraseña</label>
+                        <input type="password" className={classInputs}/>
+                        <input type="submit" value="Registrarse" className="text-black bg-white mt-6 py-1 rounded-xl text-lg from-transparent transition-all duration-300 cursor-pointer hover:bg-[#cbcbcb]"/>
                     </form>
                 </div>
             </div>    
@@ -28,4 +34,4 @@ const ModalLogin = ({openModal,setOpenModal}) => {
     );
 }
  
-export default ModalLogin;
+export default ModalRegister;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,10 +2,12 @@
 
 import Image from "next/image";
 import ModalLogin from "./modal/ModalLogin";
+import ModalRegister from "./modal/ModalRegister";
 import { useState } from "react";
 
 const Login = () => {
     const [openModalLogin,setOpenModalLogin] = useState(false);
+    const [openModalRegister,setOpenModalRegister] = useState(false);
 
     return ( 
         <>
@@ -16,11 +18,12 @@ const Login = () => {
                 <div className="flex flex-col gap-3 items-start w-[50%]">
                     <h1 className="font-bold text-7xl">Lo que está <br /> pasando ahora</h1>
                     <h2 className="font-bold text-4xl pt-14 pb-5">Únete Hoy</h2>
-                    <button className="bg-[#1a8cd8] py-1 px-20 text-lg rounded-full transition-all hover:bg-[#1b6ea5]">Crear cuenta</button>
+                    <button className="bg-[#1a8cd8] py-1 px-20 text-lg rounded-full transition-all hover:bg-[#1b6ea5]" onClick={()=>setOpenModalRegister(true)}>Crear cuenta</button>
                     <p className="py-3 text-lg">¿Ya tienes una cuenta?</p>
                     <button className="border py-1 px-20 text-lg rounded-full transition-all hover:bg-[#031018] text-[#1a8cd8]"  onClick={()=>setOpenModalLogin(true)}>Iniciar sesion</button>
                 </div>
                 <ModalLogin openModal={openModalLogin} setOpenModal={setOpenModalLogin}/>
+                <ModalRegister openModal={openModalRegister} setOpenModal={setOpenModalRegister}/>
             </main>
         </> 
     );
