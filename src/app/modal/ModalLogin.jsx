5c599cd3eacc2247778a8e222a38b57c2166987e
Jair Cornejo <jaircornejo61@gmail.com>
2024-07-12 21:53:32 -0300

    los eventos para guardar los datos de los formularios de login y register ya funcionan

--- src/app/modal/ModalLogin.jsx
+++ src/app/modal/ModalLogin.jsx
@@ -1,4 +1,4 @@
-const ModalLogin = ({openModal,setOpenModal}) => {
+const ModalLogin = ({openModal,setOpenModal,dataLogin,setDataLogin,onChange,onSubmit}) => {
     
     let verModal;
     if(openModal==false){
@@ -8,18 +8,26 @@ const ModalLogin = ({openModal,setOpenModal}) => {
         verModal="";
     }
 
+    const cerrarModal =()=>{
+        setOpenModal(false);
+        setDataLogin({
+            correo:"",
+            contrasena:""
+        });
+    }
+
     const classInputs = "border-2 border-solid border-white rounded-lg py-2 px-3 text-lg transition-all duration-300 focus:border-[#20abff] focus:outline-none";
     return ( 
         <>
             <div className={`absolute inset-0 flex items-center justify-center z-10 bg-[#242d34] bg-opacity-30 transition-all duration-500 ${verModal}`}>
                 <div className="flex flex-col pb-16 pt-9 px-24 rounded-xl">
-                    <i class="bi bi-x text-4xl text-end mb-4 cursor-pointer" onClick={()=>setOpenModal(false)}></i>
+                    <i className="bi bi-x text-4xl text-end mb-4 cursor-pointer" onClick={()=>cerrarModal()}></i>
                     <h1 className="text-center font-semibold text-4xl pb-8">Inicia sesión en X</h1>
-                    <form action="" className="flex flex-col">
+                    <form action="" className="flex flex-col" onSubmit={onSubmit}>
                         <label className="text-lg pb-4">Correo electronico</label>
-                        <input type="email" className={classInputs}/>
+                        <input type="email" className={classInputs} name="correo" onChange={onChange} value={dataLogin.correo}/>
                         <label className="text-lg mt-4 pb-4">Contraseña</label>
-                        <input type="password" className={classInputs}/>
+                        <input type="password" className={classInputs} name="contrasena" onChange={onChange} value={dataLogin.contrasena}/>
                         <input type="submit" value="Ingresar" className="text-black bg-white mt-6 py-1 rounded-xl text-lg from-transparent transition-all duration-300 cursor-pointer hover:bg-[#cbcbcb]"/>
                     </form>
                 </div>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,12 +8,40 @@ import { useState } from "react";
 const Login = () => {
     const [openModalLogin,setOpenModalLogin] = useState(false);
     const [openModalRegister,setOpenModalRegister] = useState(false);
+    const [dataFormRegister,setDataFormRegister]=useState({
+        nombre:"",
+        nombreUsuario:"",
+        correo:"",
+        contrasena:"",
+        repContrasena:"",
+    })
+    const [dataFormLogin,setDataFormLogin] = useState({
+        correo:"",
+        contrasena:""
+    })
+    const onChangeLogin = (e) => {
+        setDataFormLogin({ ...dataFormLogin, [e.target.name]: e.target.value });
+    };
+
+    const onSubmitLogin =(e)=>{
+        e.preventDefault();
+        console.log(dataFormLogin)
+    }
+
+    const onChangeRegister = (e) => {
+        setDataFormRegister({ ...dataFormRegister, [e.target.name]: e.target.value });
+    };
+
+    const onSubmitRegister =(e)=>{
+        e.preventDefault();
+        console.log(dataFormRegister)
+    }
 
     return ( 
         <>
             <main className="h-screen w-screen flex justify-between items-center">
                 <div className="w-[50%]">
-                    <Image src="/logo.png" width={500} height={300} className="w-[50%]"/>
+                    <Image src="/logo.png" width={500} height={300} className="w-[50%]" alt="logo de X"/>
                 </div>
                 <div className="flex flex-col gap-3 items-start w-[50%]">
                     <h1 className="font-bold text-7xl">Lo que está <br /> pasando ahora</h1>
@@ -22,8 +50,8 @@ const Login = () => {
                     <p className="py-3 text-lg">¿Ya tienes una cuenta?</p>
                     <button className="border py-1 px-20 text-lg rounded-full transition-all hover:bg-[#031018] text-[#1a8cd8]"  onClick={()=>setOpenModalLogin(true)}>Iniciar sesion</button>
                 </div>
-                <ModalLogin openModal={openModalLogin} setOpenModal={setOpenModalLogin}/>
-                <ModalRegister openModal={openModalRegister} setOpenModal={setOpenModalRegister}/>
+                <ModalLogin openModal={openModalLogin} setOpenModal={setOpenModalLogin} dataLogin={dataFormLogin} setDataLogin={setDataFormLogin} onChange={onChangeLogin} onSubmit={onSubmitLogin}/>
+                <ModalRegister openModal={openModalRegister} setOpenModal={setOpenModalRegister} dataRegister={dataFormRegister} setDataRegister={setDataFormRegister} onChange={onChangeRegister} onSubmit={onSubmitRegister}/>
             </main>
         </> 
     );
